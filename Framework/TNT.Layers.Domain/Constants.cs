69db2ad514ff5c100f135be9ca91beb790acea13
Trung Tran <trung.tran@yokogawa.com>
2024-07-21 14:10:57 +0700

    Refactor

--- Framework/TNT.Layers.Domain/Constants.cs
+++ Framework/TNT.Layers.Domain/Constants.cs
@@ -10,10 +10,4 @@
     {
         public const string DDMMYYYY = "dd/MM/yyyy";
     }
-
-    public static class CommonConstraints
-    {
-        public const int MaxStringLength = 255;
-        public const int IdMaxLength = 50;
-    }
 }
