7d110ad49ef4750ba7439b83acec202da1c2fabe
Trung Tran <trannamtrung1st@gmail.com>
2024-07-22 21:59:19 +0700

    Enhance

--- Framework/TNT.Layers.Domain/Exceptions/InvalidValuesException.cs
+++ Framework/TNT.Layers.Domain/Exceptions/InvalidValuesException.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using FluentValidation.Results;
 
 namespace TNT.Layers.Domain.Exceptions
 {
@@ -6,6 +7,11 @@ namespace TNT.Layers.Domain.Exceptions
     {
         public InvalidValuesException(params ValueError[] errors)
             : this((IEnumerable<ValueError>)errors)
+        {
+        }
+
+        public InvalidValuesException(IEnumerable<ValidationResult> results)
+            : this(ValueError.From(results))
         {
         }
 
@@ -29,5 +35,22 @@ namespace TNT.Layers.Domain.Exceptions
         public string ValueName { get; }
         public string ErrorCode { get; }
         public IReadOnlyDictionary<string, object> Data { get; }
+
+        public static IEnumerable<ValueError> From(ValidationResult result)
+        {
+            var errors = new List<ValueError>();
+            foreach (var error in result.Errors)
+                errors.Add(new ValueError(error.PropertyName, error.ErrorCode));
+            return errors;
+        }
+
+        public static IEnumerable<ValueError> From(IEnumerable<ValidationResult> results)
+        {
+            var errors = new List<ValueError>();
+            foreach (var result in results)
+                if (!result.IsValid)
+                    errors.AddRange(From(result));
+            return errors;
+        }
     }
 }
