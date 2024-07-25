72b16484cba8701ff415c3f65989552b3b923235
Trung Tran <trannamtrung1st@gmail.com>
2024-07-25 11:08:15 +0700

    Refactor

--- Boilerplates/TNT.Boilerplates.Common/Collections/DictionaryExtensions.cs
+++ Boilerplates/TNT.Boilerplates.Common/Collections/DictionaryExtensions.cs
@@ -1,10 +1,8 @@
-using System.Collections.Generic;
-
-namespace System.Collections
+namespace System.Collections.Generic
 {
     public static class DictionaryExtensions
     {
-        public static void AssignTo<TKey, TValue>(this IDictionary<TKey, TValue> source, IDictionary<TKey, TValue> target)
+        public static void AssignTo<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> source, IDictionary<TKey, TValue> target)
         {
             foreach (var kvp in source)
                 target[kvp.Key] = kvp.Value;
