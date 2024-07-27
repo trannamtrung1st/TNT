10fe3d3a95fc4f7bba6504474d9e72d1f4de4838
Trung Tran <trannamtrung1st@gmail.com>
2024-07-28 01:05:26 +0700

    Refactor

--- Boilerplates/TNT.Boilerplates.Resilience/ResilienceBuilder.cs
+++ Boilerplates/TNT.Boilerplates.Resilience/ResilienceBuilder.cs
@@ -2,6 +2,8 @@
 using Polly.Contrib.WaitAndRetry;
 using Polly.Retry;
 using System;
+using System.Net;
+using System.Net.Http;
 
 namespace TNT.Boilerplates.Resilience
 {
@@ -11,14 +13,14 @@ namespace TNT.Boilerplates.Resilience
         public const int DefaultFirstDelaySeconds = 2;
 
         public static IAsyncPolicy HandleTransientErrors(
-            TimeSpan? medianFirstRetryDelay = default,
-            int retryCount = DefaultRetries,
+            TimeSpan? medianFirstRetryDelay = null,
+            int? retryCount = null,
             Func<Exception, bool> exceptionFilter = null,
             Action<Exception, TimeSpan, int, Context> onRetry = null)
         {
             var jitterDelay = Backoff.DecorrelatedJitterBackoffV2(
                 medianFirstRetryDelay ?? TimeSpan.FromSeconds(DefaultFirstDelaySeconds),
-                retryCount);
+                retryCount ?? DefaultRetries);
 
             AsyncRetryPolicy commonRetry = Policy
                 .Handle(exceptionFilter ?? (ex => true))
@@ -28,15 +30,15 @@ namespace TNT.Boilerplates.Resilience
         }
 
         public static IAsyncPolicy<T> HandleTransientErrors<T>(
-            TimeSpan? medianFirstRetryDelay = default,
-            int retryCount = DefaultRetries,
+            TimeSpan? medianFirstRetryDelay = null,
+            int? retryCount = null,
             Func<T, bool> resultFilter = null,
             Func<Exception, bool> exceptionFilter = null,
             Action<DelegateResult<T>, TimeSpan, int, Context> onRetry = null)
         {
             var jitterDelay = Backoff.DecorrelatedJitterBackoffV2(
                 medianFirstRetryDelay ?? TimeSpan.FromSeconds(DefaultFirstDelaySeconds),
-                retryCount);
+                retryCount ?? DefaultRetries);
 
             var httpRetry = Policy<T>
                 .HandleResult(resultFilter ?? (result => false))
@@ -44,6 +46,22 @@ namespace TNT.Boilerplates.Resilience
                 .WaitAndRetryAsync(jitterDelay, onRetry ?? ((result, delay, count, context) => { }));
 
             return httpRetry;
+        }
+
+        public static IAsyncPolicy<HttpResponseMessage> HandleTransientHttpErrors(
+            TimeSpan? medianFirstRetryDelay = null,
+            int? retryCount = null,
+            Func<HttpResponseMessage, bool> resultFilter = null,
+            Func<Exception, bool> exceptionFilter = null,
+            Action<DelegateResult<HttpResponseMessage>, TimeSpan, int, Context> onRetry = null)
+        {
+            return HandleTransientErrors(
+                medianFirstRetryDelay,
+                retryCount,
+                resultFilter ?? (resp => (int)resp.StatusCode >= (int)HttpStatusCode.InternalServerError),
+                exceptionFilter ?? (ex => ex is HttpRequestException),
+                onRetry
+            );
         }
     }
 }
