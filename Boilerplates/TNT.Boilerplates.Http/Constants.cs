b23ec2cb9df100f47588b3914fa3119d4764850e
Trung Tran <trannamtrung1st@gmail.com>
2024-07-30 12:56:41 +0700

    Refactor media types

--- Boilerplates/TNT.Boilerplates.Http/Constants.cs
+++ Boilerplates/TNT.Boilerplates.Http/Constants.cs
@@ -1,38 +1,39 @@
-namespace TNT.Boilerplates.Http;
-
-public static class MediaTypes
+namespace TNT.Boilerplates.Http
 {
-    // Application types
-    public const string ApplicationJson = "application/json";
-    public const string ApplicationXml = "application/xml";
-    public const string ApplicationFormUrlEncoded = "application/x-www-form-urlencoded";
-    public const string ApplicationPdf = "application/pdf";
-    public const string ApplicationZip = "application/zip";
-    public const string ApplicationOctetStream = "application/octet-stream";
+    public static class MediaTypes
+    {
+        // Application types
+        public const string ApplicationJson = "application/json";
+        public const string ApplicationXml = "application/xml";
+        public const string ApplicationFormUrlEncoded = "application/x-www-form-urlencoded";
+        public const string ApplicationPdf = "application/pdf";
+        public const string ApplicationZip = "application/zip";
+        public const string ApplicationOctetStream = "application/octet-stream";
 
-    // Image types
-    public const string ImagePng = "image/png";
-    public const string ImageJpeg = "image/jpeg";
-    public const string ImageGif = "image/gif";
-    public const string ImageBmp = "image/bmp";
-    public const string ImageSvgXml = "image/svg+xml";
+        // Image types
+        public const string ImagePng = "image/png";
+        public const string ImageJpeg = "image/jpeg";
+        public const string ImageGif = "image/gif";
+        public const string ImageBmp = "image/bmp";
+        public const string ImageSvgXml = "image/svg+xml";
 
-    // Text types
-    public const string TextPlain = "text/plain";
-    public const string TextHtml = "text/html";
-    public const string TextCss = "text/css";
-    public const string TextJavascript = "text/javascript";
+        // Text types
+        public const string TextPlain = "text/plain";
+        public const string TextHtml = "text/html";
+        public const string TextCss = "text/css";
+        public const string TextJavascript = "text/javascript";
 
-    // Audio types
-    public const string AudioMpeg = "audio/mpeg";
-    public const string AudioOgg = "audio/ogg";
-    public const string AudioWav = "audio/wav";
+        // Audio types
+        public const string AudioMpeg = "audio/mpeg";
+        public const string AudioOgg = "audio/ogg";
+        public const string AudioWav = "audio/wav";
 
-    // Video types
-    public const string VideoMp4 = "video/mp4";
-    public const string VideoMpeg = "video/mpeg";
-    public const string VideoOgg = "video/ogg";
+        // Video types
+        public const string VideoMp4 = "video/mp4";
+        public const string VideoMpeg = "video/mpeg";
+        public const string VideoOgg = "video/ogg";
 
-    // Multipart types
-    public const string MultipartFormData = "multipart/form-data";
+        // Multipart types
+        public const string MultipartFormData = "multipart/form-data";
+    }
 }
